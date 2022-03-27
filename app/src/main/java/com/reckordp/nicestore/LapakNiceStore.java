a6d74b9efd2bdff123b06ecbd0bdb40fd117a2b8
Reckordp <rmuhammad134@gmail.com>
2022-03-28 06:07:41 +0700

    Add image and text setter

--- app/src/main/java/com/reckordp/nicestore/LapakNiceStore.java
+++ app/src/main/java/com/reckordp/nicestore/LapakNiceStore.java
@@ -1,12 +1,23 @@
 package com.reckordp.nicestore;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LapakNiceStore implements DetailLapak {
+    int thumbnail;
+    String teks;
+
+    LapakNiceStore(int thumbnail, String teks) {
+        this.thumbnail = thumbnail;
+        this.teks = teks;
+    }
+
     @Override
     public void performDetail(View area) {
+        ImageView image = area.findViewById(R.id.thumbnail);
+        image.setImageResource(thumbnail);
         TextView text = area.findViewById(R.id.teks);
-        text.setText("Lapak");
+        text.setText(this.teks);
     }
 }
--- app/src/main/java/com/reckordp/nicestore/MainActivity.java
+++ app/src/main/java/com/reckordp/nicestore/MainActivity.java
@@ -15,7 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<DetailLapak> deret = new ArrayList<>();
-        deret.add(new LapakNiceStore());
+        deret.add(new LapakNiceStore(R.drawable.black_japanese, "Black Japanese"));
+        deret.add(new LapakNiceStore(R.drawable.gray_japanese, "Gray Japanese"));
+        deret.add(new LapakNiceStore(R.drawable.navy_japanese, "Navy Japanese"));
+        deret.add(new LapakNiceStore(R.drawable.red_japanese, "Red Japanese"));
+        deret.add(new LapakNiceStore(R.drawable.wallet, "Wallet"));
+        deret.add(new LapakNiceStore(R.drawable.mont_blanc, "Mont Blanc"));
+        deret.add(new LapakNiceStore(R.drawable.wallet_dicky, "Wallet Dicky"));
+        deret.add(new LapakNiceStore(R.drawable.wallet_lives, "Wallet Lives"));
 
         LapakAdapter adapter = new LapakAdapter(MainActivity.this,
                 R.layout.lapak_detail, deret);
